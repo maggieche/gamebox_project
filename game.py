8e00b3014eaa2d2c3e1c52b7efe182a132acd386
cep7pa <49534525+cep7pa@users.noreply.github.com>
2019-04-16 17:21:24 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,2 +1,18 @@
 # gamebox_project
-#Cathryn Palmer cep7pa 
+
+'''basic idea: Super Mario Bros meets FireBoy and WaterGirl
+-interacting with objects and villains
+-shooting, depends on what the player touches
+-losing when coming in contact with certain objects and villains
+-different levels
+-one screen at a time- no scrolling; the player jumps from platform to platform
+-the object of the game is to get to a door either at the top or bottom of the screen, depending on where the level starts
+-for one player: use left, right, up arrows
+-for the other: use A, D, W
+-instructions screen has a map of the levels, can select with mouse
+Optional features we are including:
+*enemies (pace back and forth on a specific platform)
+*collectibles (specific to each of the two characters)
+*two players simultaneously
+*multiple levels
+'''
