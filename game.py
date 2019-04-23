f0fdfec051ed2f06daed39873aa399b855c6929f
maggieche <49534434+maggieche@users.noreply.github.com>
2019-04-23 14:51:05 -0400

    Update game.py

--- game.py
+++ game.py
@@ -35,44 +35,53 @@ import random
 
 camera = gamebox.Camera(800, 600)
 
+background = gamebox.from_image(400, 300, 'https://auburnuniforms.com/wp-content/uploads/2019/03/2019-NCAA'
+                                          '-Tournament-Court-round-2-840x469.png')
+background.height = 750
+
 # creating characters
 kyle = gamebox.from_image(400, 560, 'https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/'
                                     'players/full/4065730.png')
-kyle.height = 60
+kyle.height = 30
 kihei = gamebox.from_image(200, 560,
                            'https://a.espncdn.com/i/headshots/mens-college-basketball/players/full/4395677.png')
-kihei.height = 60
+kihei.height = 30
 
 # creating a game screen full of obstacles
 
 # level 1
-# all platforms are 20 pixels tall & 150 pixels apart (height wise)
+# all platforms are 10 pixels tall & 150 pixels apart (height wise)
 platforms_level1 = [
-    gamebox.from_color(400, 600, 'dark green', 800, 20),  # floor
-    gamebox.from_color(400, 0, 'dark green', 800, 20),  # ceiling
-    gamebox.from_color(0, 300, 'dark green', 20, 600),  # left wall
-    gamebox.from_color(800, 300, 'dark green', 20, 600),  # right wall
+    gamebox.from_color(400, 630, 'dark green', 800, 60),  # floor
+    gamebox.from_color(400, -30, 'dark green', 800, 60),  # ceiling
+    gamebox.from_color(-30, 300, 'dark green', 60, 600),  # left wall
+    gamebox.from_color(830, 300, 'dark green', 60, 600),  # right wall
 
-    gamebox.from_color(100, 450, 'dark green', 250, 20),
-    gamebox.from_color(600, 450, 'dark green', 200, 20),
-    gamebox.from_color(750, 300, 'dark green', 100, 20),
-    gamebox.from_color(250, 300, 'dark green', 300, 20),
-    gamebox.from_color(100, 150, 'dark green', 100, 20),
-    gamebox.from_color(550, 150, 'dark green', 200, 20),
+    gamebox.from_color(400, 500, 'dark green', 250, 10),
+    gamebox.from_color(650, 400, 'dark green', 200, 10),
+    gamebox.from_color(150, 400, 'dark green', 200, 10),
+    gamebox.from_color(100, 300, 'dark green', 100, 10),
+    gamebox.from_color(300, 300, 'dark green', 100, 10),
+    gamebox.from_color(500, 300, 'dark green', 100, 10),
+    gamebox.from_color(700, 300, 'dark green', 100, 10),
+    gamebox.from_color(450, 200, 'dark green', 250, 10),
+    gamebox.from_color(50, 200, 'dark green', 100, 10),
+    gamebox.from_color(200, 100, 'dark green', 200, 10),  # winning floor
+    gamebox.from_color(700, 100, 'dark green', 50, 10)
 
 ]
 
 # function for player 1 motion (to decrease repetition)
 def kylemove(keys, platformlevel):
     if pygame.K_LEFT in keys:
-        kyle.x -= 10
+        kyle.x -= 7.5
     if pygame.K_RIGHT in keys:
-        kyle.x += 10
+        kyle.x += 7.5
     if pygame.K_UP in keys:
         if platformlevel == 1:
             for platform in platforms_level1:
                 if kyle.bottom_touches(platform):
-                    kyle.speedy = -20
+                    kyle.speedy = -15
         # elif platformlevel == 2:
         #     for platform in platforms_level2:
         #         if kyle.bottom_touches(platform):
@@ -85,14 +94,14 @@ def kylemove(keys, platformlevel):
 # function for player 2 motion
 def kiheimove(keys, platformlevel):
     if pygame.K_a in keys:
-        kihei.x -= 10
+        kihei.x -= 7.5
     if pygame.K_d in keys:
-        kihei.x += 10
+        kihei.x += 7.5
     if pygame.K_w in keys:
         if platformlevel == 1:
             for platform in platforms_level1:
                 if kihei.bottom_touches(platform):
-                    kihei.speedy = -20
+                    kihei.speedy = -15
         # elif platformlevel == 2:
         #     for platform in platforms_level2:
         #         if kihei.bottom_touches(platform):
@@ -103,9 +112,9 @@ def kiheimove(keys, platformlevel):
         #             kihei.speedy = -20
 
 level = False
-level_1 = gamebox.from_color(200, 400, 'yellow', 60, 60)
-level_2 = gamebox.from_color(400, 400, 'blue', 60, 60)
-level_3 = gamebox.from_color(600, 400, 'pink', 60, 60)
+# level_1 = gamebox.from_color(200, 400, 'yellow', 60, 60)
+# level_2 = gamebox.from_color(400, 400, 'blue', 60, 60)
+# level_3 = gamebox.from_color(600, 400, 'pink', 60, 60)
 
 
 def tick(keys):
@@ -114,36 +123,39 @@ def tick(keys):
     if not level:
         # create a menu screen with different levels to choose
         camera.clear('white')
-        camera.draw(gamebox.from_text(400, 150, "Welcome to our Game! Choose Level to Start.", 50, 'black'))
-        # level logos-- eventually could be a picture not a color gamebox
-        camera.draw(level_1)
-        camera.draw(level_2)
-        camera.draw(level_3)
-        # click on level logo to start
-        if camera.mouseclick:
-            if level_1.contains(camera.mouse):
-                level = 1
-            if level_2.contains(camera.mouse):
-                level = 2
-            if level_3.contains(camera.mouse):
-                level = 3
+        camera.draw(gamebox.from_text(400, 150, "Welcome to our Game! Press 'Space' to Start.", 50, 'black'))
+        if pygame.K_SPACE in keys:
+            level = 1
+        # # level logos-- eventually could be a picture not a color gamebox
+        # camera.draw(level_1)
+        # camera.draw(level_2)
+        # camera.draw(level_3)
+        # # click on level logo to start
+        # if camera.mouseclick:
+        #     if level_1.contains(camera.mouse):
+        #         level = 1
+        #     if level_2.contains(camera.mouse):
+        #         level = 2
+        #     if level_3.contains(camera.mouse):
+        #         level = 3
 
     # level 1
     if level == 1:
         camera.clear('white')
+        camera.draw(background)
 
         # kyle movement
         kylemove(keys, 1)
         for platform in platforms_level1:
             kyle.move_to_stop_overlapping(platform)
-        kyle.speedy += 1
+        kyle.speedy += 0.75
         kyle.move_speed()
 
         # kihei movement
         kiheimove(keys, 1)
         for platform in platforms_level1:
             kihei.move_to_stop_overlapping(platform)
-        kihei.speedy += 1
+        kihei.speedy += 0.75
         kihei.move_speed()
 
         # drawing
